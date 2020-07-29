14153da0d761565faa9c15cdad219181ed234cb0
ks4na <1017086648@qq.com>
2020-07-29 20:43:17 +0800

    完成 nodejs 部分

--- node/delete-map-files.js
+++ node/delete-map-files.js
@@ -1,11 +1,46 @@
+const fsPromise = require('fs/promises')
+const path = require('path')
+
 /**
  * @description 实现这个方法，能够删除指定文件夹下的所有 `.map` 文件。
- * @param {string} p
+ * @param {string} p 路径
  * @return {void}
  */
 const deleteMapFiles = (p) => {
   // 请实现……
-};
+
+  ;(async function (p) {
+    let contents = []
+    try {
+      // 获取指定路径 p 下的目录和文件信息
+      contents = await fsPromise.readdir(p, {
+        withFileTypes: true,
+      })
+    } catch (err) {
+      console.log(`read dir '${p}' failed`, err.message)
+    }
+
+    contents.forEach(async (item) => {
+      const pathOfItem = path.resolve(p, item.name)
+
+      if (item.isDirectory()) {
+        // 如果 item 为目录， 递归调用删除方法
+        deleteMapFiles(pathOfItem)
+      } else if (path.extname(item.name) === '.map') {
+        // 如果文件以 `.map` 结尾， 直接删除
+        try {
+          await fsPromise.unlink(pathOfItem)
+          console.log(`delete file '${pathOfItem}' successfully.`)
+        } catch (err) {
+          console.log(`delete file '${pathOfItem}' failed`, err.message)
+        }
+      }
+    })
+  })(p)
+}
+
+// test
+// deleteMapFiles(path.join(__dirname, './playground'))
 
 // * ------------------------------------------------
 
